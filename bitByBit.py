bd858916d6c88a78e5a0e6a5e7aa2275b3f094ba
Astrid La Reine Lockhart <astrid.lr.lockhart@gmail.com>
2025-05-28 16:52:15 -0500

    Update bitByBit.py
    flexible version

--- bitByBit.py
+++ bitByBit.py
@@ -5,15 +5,15 @@ img = Image.open("ItDontMatter.jpg")
 
 x, y = img.size # Starting at the top left. image is 401x358px(Width(x),Height(y))
 
-for y in range (0, 358):
-    for x in range (0, 401):
-        print(f"Coords ({x}, {y}): {img.getpixel((x, y))}")
+for i in range (0, y):
+    for j in range (0, x):
+        print(f"Coords ({j}, {i}): {img.getpixel((j, i))}")
         with open('imagePxData.txt', 'a') as text:
-            if x == 400:
+            if j == int(j - 1):
                 text.write("\n")
             else:
 
-                text.write(f"{img.getpixel((x, y))}, ")
-    x = 0
+                text.write(f"{img.getpixel((j, i))}, ")
+    j = 0
 
 text.close()
