78d0c8311d87fb3e2b5349deb81b37298a687bf9
Astrid La Reine Lockhart <astrid.lr.lockhart@gmail.com>
2025-05-28 17:06:11 -0500

    Update bitByBit.py
    clean up

--- bitByBit.py
+++ bitByBit.py
@@ -1,19 +1,17 @@
 from PIL import Image
 
 # Open the image
-img = Image.open("ItDontMatter.jpg")
+img = Image.open("/home/astridlr/pythonProjects/pirateSoftwareDecrypt/PirateChallCoin/screenshotFile/screenshotOrigin.png")
 
 x, y = img.size # Starting at the top left. image is 401x358px(Width(x),Height(y))
 
-for i in range (0, y):
-    for j in range (0, x):
-        print(f"Coords ({j}, {i}): {img.getpixel((j, i))}")
+for i in range (0, y): # Looping through the rows.
+    for j in range (0, x): # Looping through the columns.
+        print(f"Coords ({j}, {i}): {img.getpixel((j, i))}") # { j is x, i is y }
         with open('imagePxData.txt', 'a') as text:
-            if j == int(j - 1):
-                text.write("\n")
-            else:
-
-                text.write(f"{img.getpixel((j, i))}, ")
+            text.write(f"{img.getpixel((j, i))}, ")
+    with open('imagePxData.txt', 'a') as text:
+        text.write("\n")
     j = 0
 
 text.close()
