c6a30e4d2acf678ffb047af8012aed3c380432aa
Astrid La Reine Lockhart <astrid.lr.lockhart@gmail.com>
2025-05-28 16:49:15 -0500

    Update bitByBit.py
    working version

--- bitByBit.py
+++ bitByBit.py
@@ -3,17 +3,17 @@ from PIL import Image
 # Open the image
 img = Image.open("ItDontMatter.jpg")
 
-x, y = 0, 0 # Starting at the top left. image is 401x358px(Width(x),Height(y))
-pixel_value = img.getpixel((x, y))
-print(f"Pixel value for({x}, {y}): {pixel_value}")
+x, y = img.size # Starting at the top left. image is 401x358px(Width(x),Height(y))
+
 for y in range (0, 358):
     for x in range (0, 401):
-        print(f"Coords ({x}, {y}): {pixel_value}")
+        print(f"Coords ({x}, {y}): {img.getpixel((x, y))}")
         with open('imagePxData.txt', 'a') as text:
             if x == 400:
                 text.write("\n")
-            else:    
-                text.write(f"{pixel_value}, ")
+            else:
+
+                text.write(f"{img.getpixel((x, y))}, ")
     x = 0
 
 text.close()
