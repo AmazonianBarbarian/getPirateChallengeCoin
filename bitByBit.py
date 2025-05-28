3af3882bbcd26db9f38e0b7f759425967947f10b
Astrid La Reine Lockhart <astrid.lr.lockhart@gmail.com>
2025-05-28 17:24:24 -0500

    Update bitByBit.py

--- bitByBit.py
+++ bitByBit.py
@@ -4,14 +4,12 @@ from PIL import Image
 img = Image.open("/home/astridlr/pythonProjects/pirateSoftwareDecrypt/PirateChallCoin/screenshotFile/screenshotOrigin.png")
 
 x, y = img.size # Starting at the top left. image is 401x358px(Width(x),Height(y))
-
+text = open('imagePxData.txt', 'w')
 for i in range (0, y): # Looping through the rows.
     for j in range (0, x): # Looping through the columns.
-        print(f"Coords ({j}, {i}): {img.getpixel((j, i))}") # { j is x, i is y }
-        with open('imagePxData.txt', 'a') as text:
-            text.write(f"{img.getpixel((j, i))}, ")
-    with open('imagePxData.txt', 'a') as text:
-        text.write("\n")
-    j = 0
+        value = img.getpixel((j, i)) # Used for efficiency. Not gonna call the same function more times than I need to.
+        print(f"Coords ({j}, {i}): {value}") # { j is x, i is y }
+        text.write(f"{value}, ")
+    text.write("\n")
 
 text.close()
